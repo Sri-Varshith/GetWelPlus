6ebcc50d44bdb91043c792f28beeb295f5ae1f4b
Sri-Varshith <nvarshith.2007@gmail.com>
2026-02-10 12:31:53 +0530

    added articles and ai chat section

--- flutter_app/lib/pages/homepage.dart
+++ flutter_app/lib/pages/homepage.dart
@@ -23,192 +23,214 @@ class HomePage extends ConsumerWidget {
   }
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('GetWel+',style: TextStyle(
-          fontSize: 31
-        ),),
-        elevation: 4,
-        centerTitle: true,
-          actions: [
-              IconButton(
-                icon: const Icon(Icons.notifications),
-                onPressed: () {
-                  // open notifications page later
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('GetWel+',style: TextStyle(
+            fontSize: 31
+          ),),
+          elevation: 4,
+          centerTitle: true,
+            actions: [
+                IconButton(
+                  icon: const Icon(Icons.notifications),
+                  onPressed: () {
+                    // open notifications page later
+                  },
+                ),
+              ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 65, 151, 69),
+                ),
+                child: StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(FirebaseAuth.instance.currentUser!.uid)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(color: Colors.white),
+                      );
+                    }
+      
+                    final data = snapshot.data!;
+                    final name = data['name'] ?? 'User';
+      
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+      
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              radius: 28,
+                              backgroundColor: Colors.black,
+                              child: Icon(
+                                Icons.person,
+                                size: 32,
+                                color: Color(0xFF4CAF50),
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            Text(
+                              name,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 44,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+      
+      
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Profile'),
+                onTap: () {
+                  Navigator.pop(context);
+                  // navigate to Profile page later
+                },
+              ),
+      
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Settings'),
+                onTap: () {
+                  Navigator.pop(context);
+                  // navigate to Settings page later
+                },
+              ),
+      
+              const Divider(),
+      
+              ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Logout'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  await FirebaseAuth.instance.signOut();
                 },
               ),
             ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 65, 151, 69),
-              ),
-              child: StreamBuilder<DocumentSnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(FirebaseAuth.instance.currentUser!.uid)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(
-                      child: CircularProgressIndicator(color: Colors.white),
+          ),
+        ),
+      
+      
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(FirebaseAuth.instance.currentUser!.uid)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+              
+                    final data = snapshot.data!;
+                    final name = data['name'] ?? 'User';
+                    final greeting = getGreeting();
+              
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '$greeting, $name 👋',
+                          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 23,
+                              ),
+                        ),
+                      ],
                     );
-                  }
-
-                  final data = snapshot.data!;
-                  final name = data['name'] ?? 'User';
-
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-
-                      Row(
-                        children: [
-                          const CircleAvatar(
-                            radius: 28,
-                            backgroundColor: Colors.black,
-                            child: Icon(
-                              Icons.person,
-                              size: 32,
-                              color: Color(0xFF4CAF50),
-                            ),
-                          ),
-                          const SizedBox(width: 12),
-                          Text(
-                            name,
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 44,
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
+                  },
+                ),
+                SizedBox(height: 20,),
+                FeatureCard(
+                  imagePath: 'assets/images/online_call.jpg',
+                  title: '1:1 Online Meet',
+                  subtitle: 'Talk privately with a psychiatrist online',
+                  onTap: () {
+                    // navigate to online consultation flow later
+                  },
+                ),
+            
+              FeatureCard(
+                imagePath: "assets/images/mood.jpg",
+                title: 'Track your mood',
+                subtitle: 'Log how you’re feeling today',
+                onTap: () {
+                  // navigate to mood tracker page
                 },
               ),
+                // SizedBox(height: 20,),
+              FeatureCard(
+                imagePath: 'assets/images/book_a_slot.jpg',
+                title: 'Book a Session',
+                subtitle: 'Meet a mental health professional offline',
+                onTap: () {
+                  // navigate to booking page later
+                },
+              ),
+            
+                FeatureCard(
+                  imagePath: 'assets/images/stress.jpg',
+                  title: 'Stress Check',
+                  subtitle: 'Measure your stress level in minutes',
+                  onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const StressCheckPage(),
+                        ),
+                      );                // open stress test page
+                  },
+                ),   
+                  FeatureCard(
+                    imagePath: 'assets/images/articles.jpg',
+                    title: 'Psychology Articles',
+                    subtitle: 'Read expert-written articles to better understand your mind',
+                    onTap: () {
+                      // navigate to articles list page
+                    },
+                  ),
+            
+            
+              ]
             ),
-
-
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Profile'),
-              onTap: () {
-                Navigator.pop(context);
-                // navigate to Profile page later
-              },
-            ),
-
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-                // navigate to Settings page later
-              },
-            ),
-
-            const Divider(),
-
-            ListTile(
-              leading: const Icon(Icons.logout),
-              title: const Text('Logout'),
-              onTap: () async {
-                Navigator.pop(context);
-                await FirebaseAuth.instance.signOut();
-              },
-            ),
-          ],
-        ),
-      ),
-
-
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            StreamBuilder<DocumentSnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(FirebaseAuth.instance.currentUser!.uid)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-          
-                final data = snapshot.data!;
-                final name = data['name'] ?? 'User';
-                final greeting = getGreeting();
-          
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '$greeting, $name 👋',
-                      style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 23,
-                          ),
-                    ),
-                  ],
-                );
-              },
-            ),
-            SizedBox(height: 20,),
-            FeatureCard(
-              imagePath: 'assets/images/online_call.jpg',
-              title: '1:1 Online Meet',
-              subtitle: 'Talk privately with a psychiatrist online',
-              onTap: () {
-                // navigate to online consultation flow later
-              },
-            ),
-
-          FeatureCard(
-            imagePath: "assets/images/mood.jpg",
-            title: 'Track your mood',
-            subtitle: 'Log how you’re feeling today',
-            onTap: () {
-              // navigate to mood tracker page
-            },
           ),
-            // SizedBox(height: 20,),
-          FeatureCard(
-            imagePath: 'assets/images/book_a_slot.jpg',
-            title: 'Book a Session',
-            subtitle: 'Meet a mental health professional offline',
-            onTap: () {
-              // navigate to booking page later
-            },
-          ),
-
-            FeatureCard(
-              imagePath: 'assets/images/stress.jpg',
-              title: 'Stress Check',
-              subtitle: 'Measure your stress level in minutes',
-              onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => const StressCheckPage(),
-                    ),
-                  );                // open stress test page
-              },
-            ),     
-
-          ]
         ),
+        floatingActionButton: FloatingActionButton(
+          shape: CircleBorder(),
+          foregroundColor: Colors.black,
+          onPressed: () {
+            // Navigate to AI Chat screen later
+          },
+          backgroundColor: Colors.green,
+          child: const Text("AI",style: TextStyle(fontSize: 27),),
+        ),
+      
+      
+      
       ),
-
-
-
     );
   }
 }
